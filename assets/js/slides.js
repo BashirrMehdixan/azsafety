b84b8a388e8e6af4a4c47ccb4c2396317f8e793c
Bashirr <beshirr096@gmail.com>
2024-11-07 01:28:33 +0400

    home services added

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,1 +1,20 @@
-new Glide('.banner_glide').mount();
+new Glide('.banner_glide', {
+    autoplay: 5000,
+    type: 'carousel'
+}).mount();
+new Glide('.services_glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    breakpoints: {
+        610: {
+            perView: 1
+        },
+        991: {
+            perView: 2
+        },
+        992: {
+            perView: 3
+        },
+    }
+}).mount()
