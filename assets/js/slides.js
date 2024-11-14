5e9af10ef46a600c5194f873fccb1f233c7aaa51
Bashirr <beshirr096@gmail.com>
2024-11-14 14:30:47 +0400

    certificates page completed

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -42,3 +42,21 @@ if (document.querySelector('.detail_glide')) {
         type: 'carousel'
     }).mount();
 }
+
+if (document.querySelector('.certificate_glide')) {
+    new Glide('.certificate_glide', {
+        type: 'slider',
+        gap: 0
+    }).mount();
+}
+
+const slide_bullets = document.querySelectorAll('.slide_pagination .p_bullet');
+const slideImg = document.querySelector('.slide_img img');
+slide_bullets.forEach(bullet => {
+    bullet.addEventListener('click', e => {
+        slide_bullets.forEach(el => el.classList.remove('active'));
+        bullet.classList.add('active');
+        slideImg.src = e.target.dataset.src;
+    })
+})
+Fancybox.bind("[data-fancybox]", {});
